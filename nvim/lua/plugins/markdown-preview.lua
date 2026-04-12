f594c10a47caac73dcab1da4641fcc2ef234fd86
Mateo Galic <mateogalic112@gmail.com>
2026-04-12 12:16:53 +0200

    remove markdown plugin

--- nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "iamcco/markdown-preview.nvim",
-    ft = { "markdown" },
-    build = "cd app && npm install",
-    keys = {
-	{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle markdown preview" },
-    },
-}
