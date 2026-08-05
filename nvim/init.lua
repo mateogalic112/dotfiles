a7c82b2f2d6c2c143528935128787642df643252
Mateo Galic <mateogalic112@gmail.com>
2026-08-05 09:09:58 +0200

    update diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,18 +17,71 @@ vim.opt.smartindent = true
 
 -- Highlight Yanks
 vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+-- Reload Files Changed Outside Neovim
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "TermClose", "TermLeave" }, {
+  group = vim.api.nvim_create_augroup("checktime", { clear = true }),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd.checktime()
+    end
   end,
 })
 
 vim.pack.add({
   "https://github.com/ellisonleao/gruvbox.nvim",
   "https://github.com/folke/snacks.nvim",
+  "https://github.com/stevearc/conform.nvim",
+  "https://github.com/neovim/nvim-lspconfig",
 })
 
+-- LSP
+vim.lsp.enable("vtsls")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+  callback = function(ev)
+    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
+      autotrigger = true,
+    })
+  end,
+})
+
+-- Diagnostics
+vim.o.signcolumn = "yes"
+
+vim.diagnostic.config({
+  virtual_text = true,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚",
+      [vim.diagnostic.severity.WARN] = "󰀪",
+      [vim.diagnostic.severity.INFO] = "󰋽",
+      [vim.diagnostic.severity.HINT] = "󰌶",
+    },
+  },
+})
+
+-- Snacks
 vim.keymap.set("n", "<leader>ff", function()
   Snacks.picker.files({ hidden = true })
 end, { desc = "Find Files" })
+
+-- Formatting
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
 
 vim.cmd.colorscheme("gruvbox")
