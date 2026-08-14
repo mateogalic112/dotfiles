21430cd269cb7cae715db9f669ceace012b0a491
Mateo Galic <mateogalic112@gmail.com>
2026-08-14 16:53:14 +0200

    update tmux focus mode

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,7 @@
 require("gruvbox").setup({
+  transparent_mode = true,
   overrides = {
     CursorLineNr = { bold = true, bg = "NONE" },
-    SignColumn = { bg = "NONE" },
-    GruvboxRedSign = { bg = "NONE" },
-    GruvboxGreenSign = { bg = "NONE" },
-    GruvboxYellowSign = { bg = "NONE" },
-    GruvboxBlueSign = { bg = "NONE" },
-    GruvboxPurpleSign = { bg = "NONE" },
-    GruvboxAquaSign = { bg = "NONE" },
-    GruvboxOrangeSign = { bg = "NONE" },
   },
 })
 
