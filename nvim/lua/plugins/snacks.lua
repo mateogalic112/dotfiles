e71a98ba75ca384cc1e39c50da23631b2c194f50
Mateo Galic <mateogalic112@gmail.com>
2026-08-17 23:10:51 +0200

    add other grep searching

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -29,10 +29,6 @@ require("snacks").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>fg", function()
-  Snacks.picker.grep()
-end, { desc = "Grep" })
-
 vim.keymap.set("n", "<leader>ff", function()
   Snacks.picker.files({ hidden = true })
 end, { desc = "Find Files" })
@@ -40,6 +36,35 @@ end, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fb", function()
   Snacks.picker.buffers()
 end, { desc = "Find buffers" })
+
+vim.keymap.set("n", "<leader>fr", function()
+  Snacks.picker.recent()
+end, { desc = "Recent Files" })
+
+vim.keymap.set("n", "<leader>sg", function()
+  Snacks.picker.grep()
+end, { desc = "Grep" })
+
+-- Visual mode greps the selection instead of the word under the cursor
+vim.keymap.set({ "n", "x" }, "<leader>sw", function()
+  Snacks.picker.grep_word()
+end, { desc = "Grep Word/Selection" })
+
+vim.keymap.set("n", "<leader>sl", function()
+  Snacks.picker.lines()
+end, { desc = "Search Buffer Lines" })
+
+vim.keymap.set("n", "<leader>sb", function()
+  Snacks.picker.grep_buffers()
+end, { desc = "Grep Open Buffers" })
+
+vim.keymap.set("n", "<leader>sd", function()
+  Snacks.picker.diagnostics()
+end, { desc = "Diagnostics" })
+
+vim.keymap.set("n", "<leader>sr", function()
+  Snacks.picker.resume()
+end, { desc = "Resume Last Picker" })
 
 vim.keymap.set("n", "<leader>e", function()
   Snacks.explorer()
