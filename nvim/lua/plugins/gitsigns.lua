a810df72f976d76ffcbdc4a300705ea586dd4965
Mateo Galic <mateogalic112@gmail.com>
2026-09-02 13:07:04 +0200

    add line blame

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,10 @@
 require("gitsigns").setup({
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text_pos = "eol",
+    delay = 200,
+  },
+
   on_attach = function(bufnr)
     local gs = require("gitsigns")
 
